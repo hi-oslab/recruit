c5996363973d8722b1a550a29dc16020a5cf2354
designerSejinOH <ohsj1999@naver.com>
2024-02-29 13:34:21 +0900

    Add gem

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -20,8 +20,8 @@ export default function Page(props) {
         onClick={() => {
           window.open('https://hioslab.com')
         }}
-        className='fixed top-0 right-0 z-20 bg-[#FFFFFF] text-[#FFFF00] text-center text-md p-2  w-fit'>
-        <span className='px-1 flex flex-row justify-center items-center gap-1 md:hover:bg-[#FFFF00] md:hover:text-[#FFFFFF] active:bg-[#FFFF00] active:text-[#FFFFFF]'>
+        className='fixed top-0 right-0 z-20 bg-[#0000FF] text-[#FFFF00] text-center text-md p-2  w-fit'>
+        <span className='px-1 flex flex-row justify-center items-center gap-1 md:hover:bg-[#FFFF00] md:hover:text-[#0000FF] active:bg-[#FFFF00] active:text-[#0000FF]'>
           hisoslab.com
         </span>
       </button>
@@ -31,16 +31,16 @@ export default function Page(props) {
             'https://docs.google.com/forms/d/e/1FAIpQLScJM7D5iMDB58d_rtXuZ6JG5-bVmuPCIU2PcRQ4MRbwfCmcNA/viewform?usp=sf_link',
           )
         }}
-        className='fixed bottom-0 right-0 z-20 bg-[#FFFFFF] text-[#FFFF00] text-center text-xl p-4  w-fit font-semibold'>
+        className='fixed bottom-0 right-0 z-20 bg-[#0000FF] text-[#FFFF00] text-center text-xl p-4  w-fit font-semibold'>
         <span className='fixed bottom-14 py-2 animate-bounce right-10 flex justify-center items-center'>
           👇👇👇👇👇
         </span>
-        <span className='active:bg-[#FFFFFF] active:border-2 active:border-[#FFFF00] active:text-[#FFFF00] md:hover:bg-[#FFFFFF] md:hover:text-[#FFFF00] md:hover:border-2 md:hover:border-[#FFFF00] px-2 py-1 bg-[#FFFF00] text-[#FFFFFF]'>
+        <span className='active:bg-[#0000FF] active:border-2 active:border-[#FFFF00] active:text-[#FFFF00] md:hover:bg-[#0000FF] md:hover:text-[#FFFF00] md:hover:border-2 md:hover:border-[#FFFF00] px-2 py-1 bg-[#FFFF00] text-[#0000FF]'>
           오픈소스랩 지원하기
         </span>
       </button>
-      <div key='top/4' className='fixed top-0 left-0 z-10 w-full h-4 text-left text-black bg-[#FFFFFF]'></div>
-      <div key='left/16' className='fixed top-0 left-0 z-10 w-16 h-full px-2 text-black bg-[#FFFFFF]'>
+      <div key='top/4' className='fixed top-0 left-0 z-10 w-full h-4 text-left text-black bg-[#0000FF]'></div>
+      <div key='left/16' className='fixed top-0 left-0 z-10 w-16 h-full px-2 text-black bg-[#0000FF]'>
         <div className='fixed z-20 origin-left -rotate-90 bottom-0 left-8 flex flex-col w-[900px] h-10 text-[#FFFF00] text-md font-semibold'>
           <div className=''> OPEN SOURCE LAB 2024-1 RECRUITING PERFORMANCE</div>
         </div>
@@ -49,17 +49,19 @@ export default function Page(props) {
         </div>
       </div>
 
-      <div key='right/4' className='fixed top-0 right-0 z-10 w-4 h-full text-left text-black bg-[#FFFFFF]'></div>
-      <div key='bottom/4' className='fixed bottom-0 left-0 z-10 w-full h-4 text-left text-black bg-[#FFFFFF]'></div>
+      <div key='right/4' className='fixed top-0 right-0 z-10 w-4 h-full text-left text-black bg-[#0000FF]'></div>
+      <div key='bottom/4' className='fixed bottom-0 left-0 z-10 w-full h-4 text-left text-black bg-[#0000FF]'></div>
       <div className='fixed z-0 w-full h-screen pointer-events-none bg-black'>
         <Scene>
+          <Common color={'#000000'} />
+          <ambientLight intensity={1} />
           <spotLight position={[50, 50, -30]} castShadow />
           <pointLight position={[-10, -10, -10]} intensity={10} />
           {/* 
           //@ts-ignore */}
-          <Stage controls={ref} environment={null} intensity={1}>
+          {/* <Stage controls={ref} environment={null} intensity={1}>
             <Model scale={5} />
-          </Stage>
+          </Stage> */}
           <OrbitControls
             ref={ref}
             autoRotate={true}
