b132b90e7d6e9f778263c0163103ec09833c05cd
designerSejinOH <ohsj1999@naver.com>
2024-03-11 09:10:26 +0900

    Add

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -113,7 +113,7 @@ export default function Page(props) {
           >
             <Arm scale={0.5} />
             <Chest scale={0.5} />
-            {/* <Foot scale={0.5} /> */}
+            <Foot scale={0.5} />
             <Hand scale={0.5} />
             <Shoulder scale={0.5} />
             <Head scale={0.5} />
